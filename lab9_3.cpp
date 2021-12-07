ab511616183c03e5bfeb2baf4eb6a4ba05840b28
Your NameCpi2544 <pokpong1789@gmail.com>
2021-12-07 18:24:25 +0700

    lab9_3.cpp completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -9,3 +9,44 @@
 "ONE-NIGHT-STAND"
 "MARRIED"
 */
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int age;
+    int height;
+    int property;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 20){
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 160){
+            cout << "Your status = UNFRIEND";
+        }else if(height >= 160 && height < 175){
+            cout << "Your status = FRIEND";
+        }else if(height >= 175){
+            cout << "Enter your property: ";
+            cin >> property;
+            if (property > 69000000){
+                cout << "Your status = MARRIED";
+            }else{
+                cout << "Your status = ONE-NIGHT-STAND";
+            }
+        }
+    }
+    if(age < 30 && age > 20){
+        cout << "Enter your property: ";
+        cin >> property;
+        if (property > 10000000){
+           cout << "Your status = BEST FRIEND"; 
+        }else if(property <= 10000000){
+            cout << "Your status = UNFRIEND";
+        }
+    }
+    if(age >= 30){
+        cout << "Your status = UNFRIEND";
+    }
+   return 0;
+}
